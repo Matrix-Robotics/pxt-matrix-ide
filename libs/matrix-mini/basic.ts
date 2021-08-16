99bdfef6a1c9c6218fbfe548156c21a0599159c3
Kevin Cheng <kevinphy@gmail.com>
2021-08-16 14:31:04 +0800

    refactoring categories

---
+++ libs/matrix-mini/basic.ts
@@ -0,0 +1,13 @@
+//% color="#00A1E9" weight=100 icon="\uf106"
+//% labelLineWidth=60
+//% subcategories=["loops"]
+namespace basic {
+    /**
+     * Exits the program to the main menu. (in the simulator restarts it)
+     */
+    //% blockId=loopstop block="exit program"
+    //% weight=10
+    //% blockGap=8
+    export function exitProgram() {
+    }
+}
---
+++ libs/matrix-mini/ns.ts
@@ -0,0 +1,9 @@
+//% subcategories=["loops"]
+namespace basic {
+
+}
+
+//% color="#b0b0b0" advanced=true weight=5
+namespace storage {
+
+}
